e8f1bcf7ffe14692841091e71aeb6c2538acd973
ashkyw <ashkyw@gmail.com>
2026-01-01 11:02:52 -0600

    updated main.py, stats.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from stats import letter_count, word_count
 
 
-def get_book_text(filepath):
+def get_book_text(filepath: str) -> str:
     with open(filepath, "r") as f:
         file_contents = f.read()
         return file_contents
@@ -13,13 +13,14 @@ def main():
     )
     book_text = get_book_text(filepath)
     num_words = word_count(book_text)
-    print("
-        ============ BOOKBOT ============
-        Analyzing book found at books/frankenstein.txt...
-        ----------- Word Count ----------
-        ")
-    print(f"Found {num_words} total words")
     total_letters = letter_count(book_text)
+    print(
+        "============ BOOKBOT ============\n",
+        "Analyzing book found at books/frankenstein.txt...\n",
+        "----------- Word Count ----------",
+    )
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     print(total_letters)
 
 
--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
-def word_count(book_text):
+def word_count(book_text: str) -> int:
     words = book_text.split()
     return len(words)
 
 
-def letter_count(book_text):
+def letter_count(book_text: str) -> dict:
     letter_count = {}
     letters = book_text.lower()
     for character in letters:
@@ -15,5 +15,14 @@ def letter_count(book_text):
     return letter_count
 
 
-def sort_dictionary(total_letters):
-    pass
+def sort_dictionary(letters: dict) -> list:
+    # return as e: num
+    # return greatest count -> least count
+    # use isalpha()
+    letter_list = []
+    for letters["character"] in letters:
+        letter_list.append(
+            letters["character"].sort(reverse=True, key=letters["character"].islapha())
+            # key=lambda letter: letter["character"]
+        )
+    print(letter_list)
